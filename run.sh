726dd43e339a425f1be2f29e1efd909ffe0e61d1
David Odell <dodell@zixcorp.com>
2019-02-06 17:46:01 -0500

    Bind to 0.0.0.0 to make things actually work.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -e
 
 docker build . -t flask-app
-docker run -p 5000:5000 -v $PWD:/app flask-app sh -c 'FLASK_APP=flask-hello.py python3 -m flask run'
+docker run -p 5000:5000 -v $PWD:/app flask-app sh -c 'FLASK_DEBUG=1 FLASK_APP=flask-hello.py python3 -m flask run --host=0.0.0.0'
